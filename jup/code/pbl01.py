0f13658873726cd76984608f63900fc4eff57e8f
ma <ma_yamaki@yahoo.com>
2020-10-29 21:59:12 +0900

    second

--- jup/code/pbl01.py
+++ jup/code/pbl01.py
@@ -1,10 +1,70 @@
 import numpy as np
 import pandas as pd
 
-sales = pd.read_csv('../data/sales_history.csv')
+train = pd.read_csv('../data/sales_history.csv')
 item_cat = pd.read_csv('../data/item_categories.csv')
 cat_name = pd.read_csv('../data/category_names.csv')
 test = pd.read_csv('../data/test.csv')
+submit = pd.read_csv(',,/data/sample_submission.csv')
 
+train.columns = 'date shop_id item_id price amount'.split()
+test.columns = 'idx item_id shop_id'.split()
 
-sales.columns = 'date shop_id item_id price amount'.split()
+def add_monthly_idx(x):
+    if x >= '2018-01-01' and x < '2018-02-01':
+        return 0
+    elif x >= '2018-02-01' and x < '2018-03-01':
+        return 1
+    elif x >= '2018-03-01' and x < '2018-04-01':
+        return 2
+    elif x >= '2018-04-01' and x < '2018-05-01':
+        return 3
+    elif x >= '2018-05-01' and x < '2018-06-01':
+        return 4
+    elif x >= '2018-06-01' and x < '2018-07-01':
+        return 5
+    elif x >= '2018-07-01' and x < '2018-08-01':
+        return 6
+    elif x >= '2018-08-01' and x < '2018-09-01':
+        return 7
+    elif x >= '2018-09-01' and x < '2018-10-01':
+        return 8
+    elif x >= '2018-10-01' and x < '2018-11-01':
+        return 9
+    elif x >= '2018-11-01' and x < '2018-12-01':
+        return 10
+    elif x >= '2018-12-01' and x < '2019-01-01':
+        return 11
+    elif x >= '2019-01-01' and x < '2019-02-01':
+        return 12
+    elif x >= '2019-02-01' and x < '2019-03-01':
+        return 13
+    elif x >= '2019-03-01' and x < '2019-04-01':
+        return 14
+    elif x >= '2019-04-01' and x < '2019-05-01':
+        return 15
+    elif x >= '2019-05-01' and x < '2019-06-01':
+        return 16
+    elif x >= '2019-06-01' and x < '2019-07-01':
+        return 17
+    elif x >= '2019-07-01' and x < '2019-08-01':
+        return 18
+    elif x >= '2019-08-01' and x < '2019-09-01':
+        return 19
+    elif x >= '2019-09-01' and x < '2019-10-01':
+        return 20
+    elif x >= '2019-10-01' and x < '2019-11-01':
+        return 21
+    else:
+        return 22
+
+train['month_idx'] = train['date'].apply(add_monthly_idx)
+
+matrix = []
+cols = ['month_idx','shop_id', 'item_id']
+
+for i in range(22):
+    sales = train[train.date==i]
+    matrix.append(np.array(list(product([i], sales.shop_id.unique(), sales.item_id.unique())), dtype='int16'))
+
+matrix = pd.DataFrame(np.vstack(matrix), columns=cols)
