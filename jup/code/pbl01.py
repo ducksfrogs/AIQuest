7350d7b3fc9c59e6faaa222a4aff453aade46300
ma <ma_yamaki@yahoo.com>
2020-10-29 17:51:55 +0900

    some

---
+++ jup/code/pbl01.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+sales = pd.read_csv('../data/sales_history.csv')
+item_cat = pd.read_csv('../data/item_categories.csv')
+cat_name = pd.read_csv('../data/category_names.csv')
+test = pd.read_csv('../data/test.csv')
+
+
+sales.columns = 
