a8218edb500266c3e492f9e127f9398edef38a28
ma <ma_yamaki@yahoo.com>
2020-11-04 20:33:10 +0900

    tulareta

---
+++ jup/code/pbl01_sec.py
@@ -0,0 +1,310 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+from xgboost import XGBRegressor
+from xgboost import plot_importance
+
+from sklearn.preprocessing import LabelEncoder
+from itertools import product
+
+import pickle
+
+
+train = pd.read_csv('../data/sales_history.csv')
+item_cat = pd.read_csv('../data/item_categories.csv')
+cat_name = pd.read_csv('../data/category_names.csv')
+test = pd.read_csv('../data/test.csv', index_col=0)
+submit = pd.read_csv(',,/data/sample_submission.csv')
+
+
+#index change
+
+
+train.columns = 'date shop_id item_id price item_cnt_day'.split()
+test.columns = 'idx item_id shop_id'.split()
+cat_name.columns = 'cat_id cat_name'.split()
+item_cat.columns = 'item_id cat_id'.split()
+
+plt.figure(figsize=(10,4))
+sns.boxplot(x=train.item_cnt_day)
+
+plt.figure(figsize=(10,4))
+sns.boxplot(x=train.price)
+
+train = train[train.price <200000]
+train = train[train.item_cnt_day < 120]
+
+train.loc[train.item_cnt_day < 0, 'item_cnt_day'] = 0
+
+def add_monthly_idx(x):
+    if x >= '2018-01-01' and x < '2018-02-01':
+        return 0
+    elif x >= '2018-02-01' and x < '2018-03-01':
+        return 1
+    elif x >= '2018-03-01' and x < '2018-04-01':
+        return 2
+    elif x >= '2018-04-01' and x < '2018-05-01':
+        return 3
+    elif x >= '2018-05-01' and x < '2018-06-01':
+        return 4
+    elif x >= '2018-06-01' and x < '2018-07-01':
+        return 5
+    elif x >= '2018-07-01' and x < '2018-08-01':
+        return 6
+    elif x >= '2018-08-01' and x < '2018-09-01':
+        return 7
+    elif x >= '2018-09-01' and x < '2018-10-01':
+        return 8
+    elif x >= '2018-10-01' and x < '2018-11-01':
+        return 9
+    elif x >= '2018-11-01' and x < '2018-12-01':
+        return 10
+    elif x >= '2018-12-01' and x < '2019-01-01':
+        return 11
+    elif x >= '2019-01-01' and x < '2019-02-01':
+        return 12
+    elif x >= '2019-02-01' and x < '2019-03-01':
+        return 13
+    elif x >= '2019-03-01' and x < '2019-04-01':
+        return 14
+    elif x >= '2019-04-01' and x < '2019-05-01':
+        return 15
+    elif x >= '2019-05-01' and x < '2019-06-01':
+        return 16
+    elif x >= '2019-06-01' and x < '2019-07-01':
+        return 17
+    elif x >= '2019-07-01' and x < '2019-08-01':
+        return 18
+    elif x >= '2019-08-01' and x < '2019-09-01':
+        return 19
+    elif x >= '2019-09-01' and x < '2019-10-01':
+        return 20
+    elif x >= '2019-10-01' and x < '2019-11-01':
+        return 21
+    else:
+        return 22
+
+train['month_idx'] = train['date'].apply(add_monthly_idx)
+
+#Feature engineering
+
+cat_name['sprit'] = cat_name['cat_name'].str.split('-')
+cat_name['type'] = cat_name['sprit'].map(lambda x: x[0].strip())
+cat_name['type-code'] = LabelEncoder().fit_transform(cat_name['type'])
+cat_name = cat_name[['cat_id','type_code']]
+
+matrix = []
+cols = ['month_idx','shop_id', 'item_id']
+
+for i in range(22):
+    sales = train[train.date==i]
+    matrix.append(np.array(list(product([i], sales.shop_id.unique(), sales.item_id.unique())), dtype='int64'))
+
+matrix = pd.DataFrame(np.vstack(matrix), columns=cols)
+matrix['month_idx'] = matrix['month_idx'].astype(np.int8)
+matrix['shop_id'] = matrix['shop_id'].astype(np.int8)
+matrix['item_id'] = matrix['item_id'].astype(np.int64)
+matrix.sort_values(cols, inplace=True)
+
+train['revenue'] = train['price'] * train['item_cnt_day']
+
+
+group = train.groupby(['month_idx', 'shop_id', 'item_id']).agg({'item_cnt_day':['sum']})
+group.columns= ['item_cnt_month']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=cols, how='left')
+matrix['item_cnt_month'] = (matrix['item_cnt_month']
+                                .fillna(0)
+                                .clip(0,20)
+                                .astype(np.float16))
+
+#Test set
+
+test['month_idx'] = 22
+test['month_idx'] = test['month_idx'].astype(np.int8)
+test['shop_id'] = test['shop_id'].astype(np.int8)
+test['item_id'] = test['item_id'].astype(np.int64)
+
+matrix = pd.concat([matrix, test], ignore_index=True, sort=False, keys=cols)
+matrix.fillna(0,inplace=True)
+
+matrix = pd.merge(matrix, item_cat, on=['item_id'], how='left')
+matrix = pd.merge(matrix, cat_name, on=['cat_id'], how='left')
+
+matrix['']
+#Target lags
+
+
+def lag_feature(df, lags, col):
+    tmp = df[['month_idx', 'shop_id','item_id', col]]
+    for i in lags:
+        shifted = tmp.copy()
+        shifted.columns = ['month_idx','shop_id', 'item_id', col+'_lag_'+str(i)]
+        shifted['month_idx'] += i
+        df = pd.merge(df, shifted, on=['month_idx','shop_id','item_id'], how='left')
+    return df
+
+matrix = lag_feature(matrix, [1,2,3,6,12], 'item_cnt_month')
+
+#Mean encorded feature
+
+
+group = matrix.groupby(['month_idx']).agg({'item_cnt_month':['mean']})
+group.columns = ['date_avg_item_cnt']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['month_idx'], how='left')
+matrix['date_avg_item_cnt'] = matrix['date_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_avg_item_cnt')
+matrix.drop(['date_avg_item_cnt'], axis=1, inplace=True)
+
+
+group = matrix.groupby(['month_idx', 'item_id']).agg({'item_cnt_month':['mean']})
+group.columns = ['date_item_avg_item_cnt']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['month_idx','item_id'], how='left')
+matrix['date_item_avg_item_cnt'] = matrix['date_item_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1,2,3,6,12], 'date_item_avg_item_cnt')
+matrix.drop(['date_item_avg_item_cnt'], axis=1, inplace=True)
+
+
+group = matrix.groupby(['month_idx','shop_id']).agg({'item_cnt_month':['mean']})
+group.columns = ['date_shop_avg_item_cnt']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['month_idx', 'shop_id'], how='left')
+matrix['date_shop_avg_item_cnt'] = matrix['date_shop_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1,2,4,6,12], 'date_shop_avg_item_cnt')
+matrix.drop(['date_shop_avg_item_cnt'], axis=1, inplace=True)
+
+
+group = matrix.groupby(['month_idx','cat_id']).agg({'item_cnt_month':['mean']})
+group.columns = ['date_cat_avg_item_cnt']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['month_idx','cat_id'], how='left')
+matrix['date_cat_avg_item_cnt'] = matrix['date_cat_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_cat_avg_item_cnt')
+matrix.drop(['date_cat_avg_item_cnt'], axis=1, inplace=True )
+
+
+group = matrix.groupby(['month_idx','shop_id','cat_id']).agg({'item_cnt_month':['mean']})
+group.columns = ['date_shop_cat_avg_item_cnt']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['month_idx','shop_id','cat_id'], how='left')
+matrix['date_shop_cat_avg_item_cnt'] = matrix['date_shop_cat_avg_item_cnt'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'date_shop_cat_avg_item_cnt')
+matrix.drop(['date_shop_cat_avg_item_cnt'], axis=1, inplace=True)
+
+
+#Trend features
+
+ts = time.time()
+
+group = train.groupby(['item_id']).agg({'price':['mean']})
+group.columns = ['item_avg_item_price']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['item_id'], how='left')
+matrix['item_avg_item_price'] = matrix['item_avg_item_price'].astype(np.float16)
+
+
+group = train.groupby(['month_idx','item_id']).agg({'price':['mean']})
+group.columns = ['date_item_avg_item_price']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['month_idx','item_id'], how='left')
+matrix['date_item_avg_item_price'] = matrix['date_item_avg_item_price'].astype(np.float16)
+
+lags = [1,2,3,4,5,6]
+matrix= lag_feature(matrix, lags, 'date_item_avg_item_price')
+
+for i in lags:
+    matrix['delta_price_lag_'+str(i)] = \
+        (matrix['date_item_avg_item_price_lag_'+str(i)] - matrix['item_avg_item_price'])/matrix['item_avg_item_price']
+
+def select_trend(row):
+    for i in lags:
+        if row['delta_price_lag_'+str(i)]:
+            return row['delta_price_lag_'+str(i)]
+    return 0
+
+
+matrix['delta_price_lag'] = matrix.apply(select_trend, axis=1)
+matrix['delta_price_lag'] = matrix['delta_price_lag'].astype(np.float16)
+matrix['delta_price_lag'].fillna(0, inplace=True)
+
+fetures_to_drop = ['item_avg_item_price','date_item_avg_item_price']
+
+for i in lags:
+    fetures_to_drop += ['date_item_avg_item_price_lag_'+str(i)]
+    fetures_to_drop += ['delta_price_lag_'+str(i)]
+
+matrix.drop(fetures_to_drop, axis=1, inplace=True)
+
+time.time() - ts
+
+#Last month shop revenue trend
+group = train.groupby(['month_idx','shop_id']).agg({'revenue':['sum']})
+group.columns = ['date_shop_revenue']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['month_idx','shop_id'], how='left')
+matrix['date_shop_revenue'] = matrix['date_shop_revenue'].astype(np.float32)
+
+
+group = group.groupby(['shop_id']).agg({'date_shop_revenue':['mean']})
+group.columns = ['shop_avg_revenue']
+group.reset_index(inplace=True)
+
+matrix = pd.merge(matrix, group, on=['shop_id'], how='left')
+matrix['shop_avg_revenue'] = matrix['shop_avg_revenue'].astype(np.float32)
+
+matrix['delta_revenue'] = (matrix['date_shop_revenue']-matrix['shop_avg_revenue'])/matrix['shop_avg_revenue']
+matrix['delta_revenue'] = matrix['delta_revenue'].astype(np.float16)
+matrix = lag_feature(matrix, [1], 'delta_revenue')
+matrix.drop(['date_shop_revenue','shop_avg_revenue','delta_revenue'], axis=1, inplace=True)
+
+
+# Special Features
+
+matrix['month'] = matrix['month_idx'] % 12
+
+days = pd.Series([31,28,31,30,31,30,31,31,30,31,30,31])
+matrix['days'] = matrix['month'].map(days).astype(np.int8)
+
+cache = {}
+matrix['item_last_sale'] = -1
+matrix['item_last_sale'] = matrix['item_last_sale'].astype(np.int8)
+for idx, row in matrix.iterrows():
+    key = row.item_id
+    if key not in cache:
+        if row.item_cnt_month !=0:
+            cache[key] = row.month_idx
+    else:
+        last_date_block_num = cache[key]
+        if row.month_idx > last_date_block_num:
+            matrix.at[idx, 'item_last_sale'] = row.month_idx - last_date_block_num
+            cache[key] = row.month_idx
+
+
+matrix['item_shop_first_sale'] = matrix['month_idx'] - matrix.groupby(['item_id','shop_id'])['month_idx'].transform('min')
+matrix['item_first_sale'] = matrix['month_idx'] - matrix.groupby('item_id')['month_idx'].transform('min')
+
+
+def fill_na(df):
+    for col in df.columns:
+        if ('_lag_' in col) & (df[col].isnull().any()):
+            if ('item_cnt' in col):
+                df[col].fillna(0, inplace=True)
+    return df
+
+
+matrix = fill_na(matrix)
+
+matrix.to_pickle('data.pkl')
