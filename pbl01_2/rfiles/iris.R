b76713c642c0b1fd569a243f2084af1e3c5665a3
ma <ma_yamaki@yahoo.com>
2020-11-18 18:37:59 +0900

    d

--- pbl01_2/rfiles/iris.R
+++ pbl01_2/rfiles/iris.R
@@ -2,4 +2,25 @@ x <- seq(-3,3,0.1)
 y <- dt(x, df=19)
 plot(x,y,type = 'l')
 qt(0.025, df=19)
+
+library(tidyverse)
 irisdata <- iris
+summary(irisdata)
+
+x <- iris
+data_mean_sd <- x %>%
+  group_by(Species) %>%
+  summarise(mean = mean(Sepal.Length), sd = sd(Sepal.Length))
+data_mean_sd
+errors <- aes(ymax=mean + sd, ymin= mean -sd)
+errors
+box <- ggplot(data = x,
+              aes(x=Species, y=Sepal.Length, fill=Species)) +
+  labs(x='', y='Sepal.Length') +
+  scale_y_continuous(expand = c(0,0), limits = c(0,ylim)) +
+  theme_classic()+
+  scale_fill_manual(values = c('gray20','gray60', 'gray80')) +
+  stat_boxplot()
+box
+boxplot(irisdata$Sepal.Length)
+ggplot(data = irisdata, aes(x=Species, y=Sepal.Length))
