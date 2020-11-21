661044de287557456e37b9f7b33594afd94c2c48
ma <ma_yamaki@yahoo.com>
2020-11-21 20:07:56 +0900

    dd

--- misc/new_machine_1.py
+++ misc/new_machine_1.py
@@ -1,1 +1,16 @@
-im
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f2(x,w):
+    return (x-w)*x*(x+2)
+
+
+x = np.linspace(-3,3,100)
+
+plt.plot(x, f2(x,2), color='black', label='Sw=2s')
+plt.plot(x, f2(x,1), color='cornflowerblue', label='Sw-1s')
+plt.legend(loc='best')
+plt.title('$f_2(x)$')
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.grid(True)
