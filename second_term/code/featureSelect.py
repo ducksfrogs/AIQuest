b02fa390516ec4fb352f23191c8904839c398ca3
ma <ma_yamaki@yahoo.com>
2021-01-11 21:35:04 +0900

    dd

--- second_term/code/featureSelect.py
+++ second_term/code/featureSelect.py
@@ -102,4 +102,62 @@ def get_oof(clf, X_train, y_train, X_test):
     oof_test = np.zeros((ntest, ))
     oof_test_skf = np.empty((NFOLDS, ntest))
 
-    
+    for i, (train_index, test_index) in enumerate(kf):
+        x_tr = X_train[train_index]
+        y_tr = y_train[train_index]
+        x_te = X_train[test_index]
+
+        clf.train(x_tr, y_tr)
+
+        oof_train[test_index] = clf.predict(x_te)
+        oof_test_skf[i, :] = clf.predict(X_test)
+    oof_test[:] = oof_test_skf.mean(axis=0)
+    return oof_train.reshape(-1,1), oof_test.reshape(-1,1)
+
+
+
+rf_params = {
+    'n_jobs': -1,
+    'n_estimators': 500,
+    'warm_start': True,
+    'max_depth': 6,
+    'min_samples_leaf': 2,
+    'max_features': 'sqrt',
+    'verbose': 0
+}
+
+et_params = {
+    'n_jobs': -1,
+    'n_estimators': 500,
+    'max_depth': 8,
+    'min_samples_leaf': 2,
+    'verbose': 0
+}
+
+ada_params = {
+    'n_estimators': 500,
+    'learning_rate': 0.7,
+}
+
+gb_params = {
+    'n_estimators': 500,
+    'max_depth': 5,
+    'min_samples_leaf': 2,
+    'verbose': 0
+}
+
+svc_params = {
+    'kernel': 'linear',
+    'C': 0.025
+}
+
+rf = SkearnHelper(clf=RandomForestClassifier, seed=SEED, params=rf_params)
+et = SkearnHelper(clf=ExtraTreesClassifier, seed=SEED, params=et_params)
+gb = SkearnHelper(clf=GradientBoostingClassifier, seed=SEED, params=gb_params)
+svc = SkearnHelper(clf=SVC, seed=SEED, params=svc_params)
+ada = sklearn(clf=AdaBoostClassifier, seed=SEED, params=ada_params)
+
+et_oof_train, et_oof_test = get_oof(et, X_train, y_train, X_test)
+rf_oof_train, rf_oof_test = get_oof(rf, X_train, y_train, X_test)
+ada_oof_train, ada_oof_test = get_oof(ada, X_train, y_train, X_test)
+gb_oof_train, gb_oof_test = get_oof(gb,X_train, y_train, X_test)
