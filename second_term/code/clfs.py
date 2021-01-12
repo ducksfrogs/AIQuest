482f7434f001d35108e502311f8ee28cf6ac4c0f
ducksfrogs <>
2021-01-12 13:41:12 +0900

    d

---
+++ second_term/code/clfs.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import glob
+import librosa
+
+files_normaly = sorted(glob.glob(os.path.abspath('../input/train/*/normal/*.wav')))
+files_anomaly = sorted(glob.glob(os.path.abspath('../input/train/*/anomaly/*.wav')))
+files_test = sorted(glob.glob(os.path.abspath('../input/test/*.wav')))
+
+normal = []
+for file in files_normaly:
+    y, sr = librosa.load(file, sr=None)
+    normal.append(y)
+
+normal = np.array(normal)
+
+anomaly = []
+for file in files_anomaly:
+    y, sr = librosa.load(file, sr=None)
+    anomaly.append(y)
+anomaly = np.array(anomaly)
+
+test = []
+for file in files_test:
+    y, sr = librosa.load(file, sr=None)
+    test.append(y)
+test = np.array(test)
+
+
+plt.plot(normal[0])
+
+plt.plot(anomaly[0])
+
+melspec_normal = []
+for n in normal:
+    m = librosa.feature.melspectrogram(n, n_mels=256)
+    m = librosa.power_to_db(m).astype(np.float32)
+    melspec_normal.append(m)
+melspec_normal = np.array(melspec_normal)
+
+melspec_anomaly = []
+for a in anomaly:
+    m = librosa.feature.melspectrogram(a, n_mels=256)
+    m = librosa.power_to_db(m).astype(np.float32)
+    melspec_anomaly.append(m)
+melspec_anomaly = np.array(melspec_anomaly)
+
+melspec_test = []
+for t in test:
+    m = librosa.feature.melspectrogram(t, n_mels=256)
+    m = librosa.power_to_db(m).astype(np.float32)
+    melspec_test.append(m)
+melspec_test = np.array(melspec_test)
+
+
+train = np.concatenate([melspec_normal, melspec_anomaly])
+train = train.reshape(train.shape[0],-1)
+test = melspec_test.reshape(melspec_test.shape[0], -1)
+
+target = np.concatenate([np.zeros(len(melspec_normal)), np.ones(len(melspec_anomaly))])
+
+from sklearn.model_selection import train_test_split
+
+train_X, test_X, train_Y, test_Y = train_test_split(train, target, test_size=0.33, random_state=42)
+
+
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+scaler.fit(train_X)
+train_X_scale = scaler.transform(train_X)
+test_X_scale = scaler.transform(test_X)
+test_scale = scaler.transform(test)
+
+from sklearn.svm import SVC
+
+clf = SVC()
+
+clf.fit(train_X, train_Y)
+clf.score(test_X, test_Y)
